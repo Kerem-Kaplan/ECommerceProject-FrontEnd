d4294de15fb2afbf064610ac4eb6167adef8f115
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-02-01 17:24:08 +0300

    added alertifyjs library and customized

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AlertifyService, MessageType, Position } from './../../services/admin/alertify.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
+  
+  constructor(private alertify:AlertifyService) {}
+  
+  
+  ngOnInit(): void {
+    this.alertify.message("Merhaba",{
+      messageType:MessageType.Success,
+      delay:3,
+      position:Position.TopCenter
 
+    })
+    
+  }
+  d(){
+    this.alertify.dismiss()
+  }
+
+  
+  
 }
