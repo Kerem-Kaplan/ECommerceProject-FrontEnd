6936bac6e082221b8fa25e469971d1549d636f8f
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-02-21 12:38:46 +0300

    edited product service methods

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,4 +1,3 @@
-import { Product } from './../../../contracts/product';
 import { HttpClientService } from './../../../services/common/http-client.service';
 import { BaseComponent, SpinnerType } from './../../../base/base.component';
 import { Component, OnInit } from '@angular/core';
@@ -17,43 +16,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
   
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom)
-    
-    
-    
-    this.httpClientService.get<Product[]>({
-      controller:"products"
-    }).subscribe(data=>{console.log(data) })
-
-    // this.httpClientService.post({
-    //   controller:"products"
-    // },{
-    //   name:"Kalem",
-    //   stock:100,
-    //   price:15
-    // }).subscribe()
-
-    // this.httpClientService.put({
-    //   controller:"products"
-    // },{
-    //   id:"34ee4f96-af79-42ed-be79-7531f801e702",
-    //   name:"Silgi",
-    //   stock:15,
-    //   price:31
-    // }).subscribe()
-    
-
-    // this.httpClientService.delete({
-    //   controller:"products"
-    // },"34ee4f96-af79-42ed-be79-7531f801e702"
-    // ).subscribe()
-
-
-  //   this.httpClientService.get({
-  //     fullEndpoint:"https://jsonplaceholder.typicode.com/posts"
-  //   }).subscribe(data=>console.log(data))
-
-
    }
 
 }
