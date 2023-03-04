f3627c95df23df8d57d043e5c159d18df8c4fe73
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-03-04 20:49:44 +0300

    added delete modal dialog for delete directive

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,7 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { AlertifyService, MessageType, Position } from './../../services/admin/alertify.service';
+import { HttpClientService } from './../../services/common/http-client.service';
+import { DeleteDialogComponent, DeleteState } from './../../dialogs/delete-dialog/delete-dialog.component';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { BaseComponent, SpinnerType } from './../../base/base.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
 import { Directive,ElementRef,Renderer2,HostListener, Input, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SpinnerType } from 'src/app/base/base.component';
 
 declare var $:any
 
@@ -13,8 +18,10 @@ export class DeleteDirective {
   constructor(
     private element:ElementRef,
     private _renderer:Renderer2,
-    private productService:ProductService,
-    private spinner:NgxSpinnerService
+    private httpClientService:HttpClientService,
+    private spinner:NgxSpinnerService,
+    public dialog:MatDialog,
+    private alertifyService:AlertifyService
     ){
       const img=_renderer.createElement("img")
       img.setAttribute("src","../../../assets/delete.png")
@@ -24,19 +31,56 @@ export class DeleteDirective {
      }
 
      @Input() id:string
+     @Input() controller:string
      @Output() callback:EventEmitter<any> =new EventEmitter()
 
      @HostListener("click")
      async onClick(){
-      this.spinner.show(SpinnerType.BallAtom)
-      const td:HTMLTableColElement=this.element.nativeElement
-      await this.productService.delete(this.id)
-      $(td.parentElement).fadeOut(2000,()=>{
-        this.callback.emit()
-      })
-      this.spinner.hide(SpinnerType.BallAtom,2000)
+      this.openDialog(async()=>{
+        this.spinner.show(SpinnerType.BallAtom)
+        const td:HTMLTableColElement=this.element.nativeElement
+      this.httpClientService.delete({
+        controller:this.controller
+      },this.id).subscribe(data=>{
 
+        $(td.parentElement).animate({
+          opacity:0,
+          left:"+=50",
+          height:"toggle"
+        },1000,()=>{
+          this.callback.emit()
+          this.spinner.hide(SpinnerType.BallAtom,1500)
+          this.alertifyService.message("Ürün başarıyla silinmiştir",{
+            dismissOthers:true,
+            messageType:MessageType.Success,
+            position:Position.TopCenter
+          })
+        })
+      },(errorResponse:HttpErrorResponse)=>{
+        this.spinner.hide(SpinnerType.BallAtom,1500)
+        this.alertifyService.message("Hata alındı ve ürün silinmedi",{
+          dismissOthers:true,
+          messageType:MessageType.Error,
+          position:Position.TopCenter
+        })
+      })    
+    })
       
-     }
+      
+  }
+
+     openDialog(afterClosed:any): void {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        width:'250px',
+        data: DeleteState.Yes,
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if(result==DeleteState.Yes){
+          afterClosed()
+        }
+       
+      });
+    }
 
 }
